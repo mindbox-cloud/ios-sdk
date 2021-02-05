8ce7a4920c3aced2dbdfc2cd6b97ae098dab3d0c
Maksim Kazachkov <maksim.kazachkov@umbrellait.com>
2021-02-05 08:52:31 +0300

    Updated CDEvent entity with retryTimestamp
    Updated CDEvent fetchRequest

--- MindBox/Database/Entities/CDEvent+CoreDataProperties.swift
+++ MindBox/Database/Entities/CDEvent+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  CDEvent+CoreDataProperties.swift
 //  MindBox
 //
-//  Created by Maksim Kazachkov on 04.02.2021.
+//  Created by Maksim Kazachkov on 05.02.2021.
 //  Copyright © 2021 Mikhail Barilov. All rights reserved.
 //
 //
@@ -12,10 +12,13 @@ import CoreData
 
 
 extension CDEvent {
-
+    
     @nonobjc public class func fetchRequest() -> NSFetchRequest<CDEvent> {
         let request = NSFetchRequest<CDEvent>(entityName: "CDEvent")
-        request.sortDescriptors = [NSSortDescriptor(key: #keyPath(CDEvent.timestamp), ascending: true)]
+        request.sortDescriptors = [
+            NSSortDescriptor(key: #keyPath(CDEvent.timestamp), ascending: true),
+            NSSortDescriptor(key: #keyPath(CDEvent.retryTimestamp), ascending: true)
+        ]
         return request
     }
     
@@ -24,10 +27,11 @@ extension CDEvent {
         request.predicate = NSPredicate(format: "%K == %@", [#keyPath(CDEvent.transactionId), transactionId])
         return request
     }
-
+    
     @NSManaged public var body: String?
     @NSManaged public var timestamp: Double
     @NSManaged public var transactionId: String?
     @NSManaged public var type: String?
-
+    @NSManaged public var retryTimestamp: Double
+    
 }
