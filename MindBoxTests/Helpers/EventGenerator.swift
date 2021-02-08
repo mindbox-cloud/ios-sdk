c69434207398c91dc84fe151c8bb84eb3f3d015c
Maksim Kazachkov <maksimkn.94@gmail.com>
2021-02-08 14:53:45 +0300

    Implemented EventGenerator

---
+++ MindBoxTests/Helpers/EventGenerator.swift
@@ -0,0 +1,39 @@
+//
+//  EventGenerator.swift
+//  MindBoxTests
+//
+//  Created by Maksim Kazachkov on 08.02.2021.
+//  Copyright © 2021 Mikhail Barilov. All rights reserved.
+//
+
+import Foundation
+@testable import MindBox
+
+struct EventGenerator {
+
+    func generateEvent() -> Event {
+        Event(
+            transactionId: UUID().uuidString,
+            enqueueTimeStamp: Date().timeIntervalSince1970,
+            type: .installed,
+            body: randomString()
+        )
+    }
+    
+    func generateEvents(count: Int) -> [Event] {
+        return (0...count).map { _ in
+            Event(
+                transactionId: UUID().uuidString,
+                enqueueTimeStamp: Date().timeIntervalSince1970,
+                type: .installed,
+                body: randomString()
+            )
+        }
+    }
+    
+    private func randomString(length: Int = 10) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return String((0..<length).map{ _ in letters.randomElement()! })
+    }
+    
+}
