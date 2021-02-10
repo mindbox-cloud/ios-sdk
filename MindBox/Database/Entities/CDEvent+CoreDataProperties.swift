b243bca91642e27d28773e1b297aab28ddba4252
Maksim Kazachkov <maksimkn.94@gmail.com>
2021-02-10 13:31:09 +0300

    Implemented unretry/retry fetchRequests

--- MindBox/Database/Entities/CDEvent+CoreDataProperties.swift
+++ MindBox/Database/Entities/CDEvent+CoreDataProperties.swift
@@ -15,12 +15,46 @@ extension CDEvent {
     
     @nonobjc public class func fetchRequest() -> NSFetchRequest<CDEvent> {
         let request = NSFetchRequest<CDEvent>(entityName: "CDEvent")
-        if let monthLimitDateStamp = monthLimitDate?.timeIntervalSince1970 {
+        if let monthLimitDateStamp = lifeLimitDate?.timeIntervalSince1970 {
             request.predicate = NSPredicate(format: "%K > %@", argumentArray: [#keyPath(CDEvent.timestamp), monthLimitDateStamp])
         }
         request.sortDescriptors = [
+            NSSortDescriptor(key: #keyPath(CDEvent.retryTimestamp), ascending: true),
             NSSortDescriptor(key: #keyPath(CDEvent.timestamp), ascending: true),
-            NSSortDescriptor(key: #keyPath(CDEvent.retryTimestamp), ascending: true)
+        ]
+        return request
+    }
+    
+    @nonobjc public class func unretryFetchRequest() -> NSFetchRequest<CDEvent> {
+        let request = NSFetchRequest<CDEvent>(entityName: "CDEvent")
+        if let lifetimeLimitTimestamp = lifeLimitDate?.timeIntervalSince1970 {
+            request.predicate = NSCompoundPredicate(type: .and, subpredicates: [
+                NSPredicate(format: "%K == nil", argumentArray: [#keyPath(CDEvent.retryTimestamp)]),
+                NSPredicate(format: "%K > %@", argumentArray: [#keyPath(CDEvent.timestamp), lifetimeLimitTimestamp])
+            ])
+        }
+        request.sortDescriptors = [
+            NSSortDescriptor(key: #keyPath(CDEvent.timestamp), ascending: true),
+        ]
+        return request
+    }
+    
+    @nonobjc public class func retryFetchRequest(deadline: TimeInterval) -> NSFetchRequest<CDEvent> {
+        let request = NSFetchRequest<CDEvent>(entityName: "CDEvent")
+        if let monthLimitDateStamp = lifeLimitDate?.timeIntervalSince1970 {
+            var subpredicates: [NSPredicate] = [
+                NSPredicate(format: "%K != nil", argumentArray: [#keyPath(CDEvent.retryTimestamp)]),
+                NSPredicate(format: "%K > %@", argumentArray: [#keyPath(CDEvent.timestamp), monthLimitDateStamp])
+            ]
+            if let deadlineDate = Calendar.current.date(byAdding: .second, value: -Int(deadline), to: Date()) {
+                subpredicates.append(
+                    NSPredicate(format: "%K < %@", argumentArray: [#keyPath(CDEvent.retryTimestamp), deadlineDate.timeIntervalSince1970])
+                )
+            }
+            request.predicate = NSCompoundPredicate(type: .and, subpredicates: subpredicates)
+        }
+        request.sortDescriptors = [
+            NSSortDescriptor(key: #keyPath(CDEvent.retryTimestamp), ascending: true),
         ]
         return request
     }
@@ -33,13 +67,13 @@ extension CDEvent {
     
     public class func deprecatedEventsFetchRequest() -> NSFetchRequest<CDEvent> {
         let request = NSFetchRequest<CDEvent>(entityName: "CDEvent")
-        if let monthLimitDateStamp = monthLimitDate?.timeIntervalSince1970 {
+        if let monthLimitDateStamp = lifeLimitDate?.timeIntervalSince1970 {
             request.predicate = NSPredicate(format: "%K <= %@", argumentArray: [#keyPath(CDEvent.timestamp), monthLimitDateStamp])
         }
         return request
     }
     
-    static var monthLimitDate: Date? {
+    static var lifeLimitDate: Date? {
         let calendar: Calendar = .current
         guard let monthLimitDate = calendar.date(byAdding: .month, value: -6, to: Date()) else {
             return nil
