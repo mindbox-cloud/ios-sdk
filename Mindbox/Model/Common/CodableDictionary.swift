86cf07e849d161c139752800d3ffdd5d2f53845a
Ihor Kandaurov <lbrdev.contact@gmail.com>
2021-05-24 12:00:04 +0300

    classes update

--- Mindbox/Model/Common/CodableDictionary.swift
+++ Mindbox/Model/Common/CodableDictionary.swift
@@ -14,52 +14,24 @@ import Foundation
 public struct CodableDictionary: Codable {
     public typealias DictionaryType = [String: Any]
 
-    private var value: Dictionary<String, Any>
+    private var value: JSON
 
     public init(_ value: [String: Any]) {
-        self.value = value
+        self.value = JSON(value)
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        let data = try container.decode(Data.self)
-        guard let value = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
-            throw CustomFieldsError.decodingError
-        }
-        self.value = value
+        value = try container.decode(JSON.self)
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
-        let data = try JSONSerialization.data(withJSONObject: value, options: [])
-        try container.encode(data)
+        try container.encode(value)
     }
 
     public enum CustomFieldsError: Error {
         case decodingError
-    }
-}
-
-// MARK: - Collection
-
-extension CodableDictionary: Collection {
-    public typealias Index = DictionaryType.Index
-    public typealias Element = DictionaryType.Element
-
-    public var startIndex: Index {
-        return value.startIndex
-    }
-
-    public var endIndex: Index {
-        return value.endIndex
-    }
-
-    public subscript(index: Index) -> Iterator.Element {
-        return value[index]
-    }
-
-    public func index(after i: CustomFields.DictionaryType.Index) -> CustomFields.DictionaryType.Index {
-        return value.index(after: i)
     }
 }
 
@@ -70,15 +42,16 @@ extension CodableDictionary: ExpressibleByDictionaryLiteral {
     public typealias Value = Any
 
     public init(dictionaryLiteral elements: (String, Any)...) {
-        value = elements.reduce(into: [:]) { $0[$1.0] = $1.1 }
+        let stringAny = elements.reduce(into: [:]) { $0[$1.0] = $1.1 }
+        value = JSON(stringAny)
     }
 }
 
 // MARK: - Subscript
 
-extension CodableDictionary {
-    subscript(key: String) -> Any {
-        get { return value[key] ?? [] }
-        set { value[key] = newValue }
-    }
-}
+// extension CodableDictionary {
+//    subscript(key: String) -> Any {
+//        get { return value[key] }
+//        set { value[key] = newValue }
+//    }
+// }
--- Mindbox/Model/Request/DiscountRequest.swift
+++ Mindbox/Model/Request/DiscountRequest.swift
@@ -3,17 +3,25 @@ import Foundation
 open class DiscountRequest: Encodable {
     public var type: DiscountTypeRequest?
     public var promoCode: DiscountPromoCodeRequest?
+    public var externalPromoAction: DiscountExternalPromoActionRequest?
     public var amount: Decimal?
-    public var externalPromoAction: AreaRequest?
 
     public init(
         type: DiscountTypeRequest? = nil,
         promoCode: DiscountPromoCodeRequest? = nil,
-        amount: Decimal? = nil,
-        externalPromoAction: AreaRequest? = nil
+        amount: Decimal? = nil
     ) {
         self.type = type
         self.promoCode = promoCode
+        self.amount = amount
+    }
+
+    public init(
+        type: DiscountTypeRequest? = nil,
+        externalPromoAction: DiscountExternalPromoActionRequest? = nil,
+        amount: Decimal? = nil
+    ) {
+        self.type = type
         self.amount = amount
         self.externalPromoAction = externalPromoAction
     }
