56533194d6e8f065f2301e16dfa528ac71c9f0c5
Mikhail Plotnikov <mikhail.plotnikov@umbrellait.com>
2021-07-08 17:31:51 +0300

    Removed duplicates

--- Mindbox/Model/Response/DiscountCardResponse.swift
+++ Mindbox/Model/Response/DiscountCardResponse.swift
@@ -4,5 +4,5 @@ open class DiscountCardResponse: Decodable {
     public let ids: IDS?
     public let customFields: CustomFields?
     public let status: StatusResponse?
-    public let type: TypeResponse?
+    public let type: BalanceTypeReponse?
 }
