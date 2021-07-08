c31165fefb5853c21034d7b3114518de0565fc5c
Mikhail Plotnikov <mikhail.plotnikov@umbrellait.com>
2021-07-08 10:40:51 +0300

    Fixed response-parsing

---
+++ Mindbox/Model/Response/RetailOrderStatisticsResponse.swift
@@ -0,0 +1,12 @@
+//
+//  RetailOrderStatisticsResponse.swift
+//  Mindbox
+//
+//  Created by Mikhail Plotnikov on 08.07.2021.
+//
+
+import Foundation
+
+open class RetailOrderStatisticsResponse: Decodable {
+    public let totalPaidAmount: Double?
+}
