f2cec7d3f7543f9d8a026f014d86eb6546b71eb0
Mikhail Plotnikov <mikhail.plotnikov@umbrellait.com>
2021-07-09 09:25:53 +0300

    Fix poolDescriptor

--- Mindbox/Model/Response/PoolResponse.swift
+++ Mindbox/Model/Response/PoolResponse.swift
@@ -4,4 +4,10 @@ open class PoolResponse: Decodable {
     public let ids: IDS?
     public let name: String?
     public let poolDescription: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case ids
+        case name
+        case poolDescription = "description"
+      }
 }
