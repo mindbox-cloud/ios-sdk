b19b072e08429bc6d471dd08f42edb2eadfddba1
Plotnikov Mikhail <mikhail.plotnikov@umbrellait.com>
2021-12-08 14:41:36 +0300

    test package

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Mindbox",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Mindbox",
             targets: ["Mindbox"]),
@@ -15,19 +13,15 @@ let package = Package(
             targets: ["MindboxNotifications"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Mindbox",
             dependencies: [],
-            path: "ios-sdk/Mindbox"),
+            path: "/Mindbox"),
         .testTarget(
             name: "MindboxNotifications",
             dependencies: [],
-            path: "ios-sdk/MindboxNotifications"),
+            path: "/MindboxNotifications"),
     ]
 )
