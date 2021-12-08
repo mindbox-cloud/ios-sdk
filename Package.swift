843fc60267e545fe2200d83808fc72cbb8196024
Plotnikov Mikhail <mikhail.plotnikov@umbrellait.com>
2021-12-08 14:31:57 +0300

    fix package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ios-sdk",
+    name: "Mindbox",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -24,10 +24,10 @@ let package = Package(
         .target(
             name: "Mindbox",
             dependencies: [],
-            path: "Mindbox"),
+            path: "Mindbox/Mindbox"),
         .testTarget(
             name: "MindboxNotifications",
             dependencies: [],
-            path: "MindboxNotifications"),
+            path: "Mindbox/MindboxNotifications"),
     ]
 )
