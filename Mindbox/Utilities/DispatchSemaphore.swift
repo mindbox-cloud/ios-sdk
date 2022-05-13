3ffe4e18848854f819d32588a8efbd47123b77b5
Pavel <pavel.zavarin@umbrellait.com>
2022-05-13 13:24:17 +0300

    Thread-safety for receiving APNS Token and Device UUID

---
+++ Mindbox/Utilities/DispatchSemaphore.swift
@@ -0,0 +1,19 @@
+//
+//  DispatchSemaphore.swift
+//  Mindbox
+//
+//  Created by Pavel Zavarin on 13.05.2022.
+//  Copyright © 2022 Mikhail Barilov. All rights reserved.
+//
+
+import Foundation
+
+extension DispatchSemaphore {
+    
+    func lock<T>(execute task: () throws -> T) rethrows -> T {
+        wait()
+        defer { signal() }
+        return try task()
+    }
+    
+}
