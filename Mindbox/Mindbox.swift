f4b8bd938e73f2bb02f02f40a814e9f6fa23efb3
Max Kazakov <kazakov.maxim.a@icloud.com>
2022-09-08 16:54:39 +0500

    [MBX-29] Moved start call to SDK initialization point

--- Mindbox/Mindbox.swift
+++ Mindbox/Mindbox.swift
@@ -65,6 +65,7 @@ public class Mindbox: NSObject {
      */
     public func initialization(configuration: MBConfiguration) {
         coreController?.initialization(configuration: configuration)
+        inAppMessagesManager?.start()
     }
 
     private var observeTokens: [UUID] = []
@@ -517,8 +518,7 @@ public class Mindbox: NSObject {
                     .category(.general).level(.fault).make()
                 self.initError = error
             }
-            self.persistenceStorage?.storeToFileBackgroundExecution()
-            self.inAppMessagesManager?.start()
+            self.persistenceStorage?.storeToFileBackgroundExecution()            
         }
     }
 
