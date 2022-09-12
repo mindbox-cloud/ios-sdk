9b7c6c35b9df96baa2e00f831ca41b7433381b2b
Max Kazakov <kazakov.maxim.a@icloud.com>
2022-09-12 12:24:23 +0500

    [MBX-31] Segmentation check logic

--- Mindbox/InAppMessages/PresentChecker/InAppSegmentationChecker.swift
+++ Mindbox/InAppMessages/PresentChecker/InAppSegmentationChecker.swift
@@ -12,21 +12,87 @@ import Foundation
 final class InAppSegmentationChecker {
 
     func getInAppToPresent(request: InAppsCheckRequest, completionQueue: DispatchQueue, _ completion: @escaping (InAppResponse?) -> Void) {
-        // make network request to get if there're in apps to show for the client
-        switch request.triggerEvent {
-        case .start:
-            completionQueue.async {
-                if let firstStartInApp = request.possibleInApps.first {
-                    completion(InAppResponse(triggerEvent: request.triggerEvent, inAppToShowId: firstStartInApp.inAppId))
-                } else {
-                    completion(nil)
-                }
-            }
+//        POST https://api(-staging).mindbox.ru/v3/operations/sync?endpointId={Идентификатор точки интеграции}&operation=Tracker.CheckCustomerSegments&deviceUUID={UUID устройства}
+//
+//    Headers:
+//        Content-Type: application/json; charset=utf-8
+//    Accept: application/json
+//        User-Agent: {User-Agent устройства клиента}
+//        X-Customer-IP: {IP адрес клиента}
 
-        case .applicationEvent:
-            completionQueue.async {
+        let targetings = request.possibleInApps.compactMap { $0.targeting }
+        let segmentationRequest = SegmentationCheckRequest(
+            segmentations: targetings.map {
+                SegmentationCheckRequest.Segmentation(
+                    ids: SegmentationCheckRequest.Segmentation.Id(externalId: $0.segmention)
+                )
+            }
+        )
+        fetchSegments(segmentationCheckRequest: segmentationRequest) { response in
+            guard let response = response,
+                  response.status == .success else { return }
+
+            // [SegmentationId: SegmentId] from response
+            let customerSegmentationDict: [String: String] = Dictionary(
+                uniqueKeysWithValues: response.customerSegmentations.compactMap {
+                    guard let segment = $0.segment?.ids else { return nil }
+                    return ($0.segmentation.ids.externalId, segment.externalId)
+                }
+            )
+
+            if let inAppToShow = request.possibleInApps.first(where: { inApp in
+                guard let targeting = inApp.targeting else { return false }
+                return customerSegmentationDict[targeting.segmention] == targeting.segment
+            }) {
+                completion(InAppResponse(triggerEvent: request.triggerEvent, inAppToShowId: inAppToShow.inAppId))
+            } else {
                 completion(nil)
             }
         }
+    }
+
+    private func fetchSegments(segmentationCheckRequest: SegmentationCheckRequest, completion: @escaping (SegmentationCheckResponse?) -> Void) {
+
+    }
+}
+
+
+struct SegmentationCheckRequest: Encodable {
+    let segmentations: [Segmentation]
+
+    struct Segmentation: Encodable {
+        let ids: Id
+
+        struct Id: Encodable {
+            let externalId: String
+        }
+    }
+}
+
+struct SegmentationCheckResponse: Decodable {
+    enum Status: String, Decodable {
+        case success = "Success"
+        case validationError = "ValidationError"
+        case protocolError = "ProtocolError"
+    }
+
+    let status: Status?
+    let customerSegmentations: [CustomerSegmentation]
+
+    struct CustomerSegmentation: Decodable {
+        let segmentation: Segmentation
+        let segment: Segment?
+    }
+
+    struct Segmentation: Decodable {
+        let ids: Id
+    }
+
+    struct Segment: Decodable {
+        let ids: Id?
+    }
+
+    struct Id: Decodable {
+        let externalId: String
     }
 }
