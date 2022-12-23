a509ec98a6ad3ed480e83041aa3e49a6062b2690
Akylbek Utekeshev <akylbek@workingeeks.com>
2022-12-23 17:44:33 +0600

    InAppConfigResponse with new targeting

--- Mindbox/InAppMessages/Configuration/API/InAppConfigResponse.swift
+++ Mindbox/InAppMessages/Configuration/API/InAppConfigResponse.swift
@@ -8,105 +8,83 @@
 import Foundation
 
 struct InAppConfigResponse: Decodable {
+    let inapps: [FailableDecodable<InApp>]
+}
 
-    struct SdkVersion: Decodable {
-        let min: Int
-        let max: Int?
-    }
-
+extension InAppConfigResponse {
     struct InApp: Decodable {
         let id: String
         let sdkVersion: SdkVersion
         let targeting: InAppTargeting
         let form: InAppFormVariants
     }
+}
 
-    struct SegmentationTargeting: Decodable {
-        let segment: String?
-        let segmentation: String?
+// MARK: - SdkVersion
+extension InAppConfigResponse {
+    struct SdkVersion: Decodable {
+        let min: Int
+        let max: Int?
     }
+}
 
+// MARK: - InAppTargeting
+extension InAppConfigResponse {
     struct InAppTargeting: Decodable {
-        init(type: InAppConfigResponse.InAppTargetingType, payload: InAppConfigResponse.SegmentationTargeting) {
+        let type: TargetingType
+        let nodes: [TargetingNode]?
+        
+        init(type: TargetingType, nodes: [TargetingNode]?) {
             self.type = type
-            self.payload = payload
+            self.nodes = nodes
         }
-
-        let type: InAppTargetingType
-        let payload: SegmentationTargeting
-
+        
+        enum CodingKeys: String, CodingKey {
+            case type = "$type"
+            case nodes
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            self.type = try container.decode(TargetingType.self, forKey: .type)
+            self.nodes = try container.decode([TargetingNode].self, forKey: .nodes)
+        }
+    }
+    
+    struct TargetingNode: Decodable {
+        let type: TargetingType
+        
         enum CodingKeys: String, CodingKey {
             case type = "$type"
         }
-
-        init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            self.type = try container.decode(InAppTargetingType.self, forKey: CodingKeys.type)
-            switch type {
-            case .simple:
-                let payload = try SegmentationTargeting(from: decoder)
-                if (payload.segment == nil && payload.segmentation == nil) || (payload.segment != nil && payload.segmentation != nil) {
-                    self.payload = payload
-                } else {
-                    throw DecodingError.dataCorrupted(
-                        .init(
-                            codingPath: decoder.codingPath,
-                            debugDescription: "Segment and segmentation in payload should be either both nil or both not nill"
-                        )
-                    )
-                }
-            }
-        }
     }
-
-    struct InAppForm: Decodable {
-        init(payload: InAppConfigResponse.InAppFormPayload? = nil) {
-            self.payload = payload
-        }
-
-        enum CodingKeys: String, CodingKey {
-            case type = "$type"
-        }
-
-        let payload: InAppFormPayload?
-
-        init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            guard let type = try? container.decode(InAppFormType.self, forKey: .type) else {
-                self.payload = nil
-                return
-            }
-            switch type {
-            case .simpleImage:
-                let simpleImagePayload = try SimpleImageInApp(from: decoder)
-                self.payload = .simpleImage(simpleImagePayload)
-            }
-        }
+    
+    enum TargetingType: String, Decodable {
+        case and
+        case or
+        case `true`
     }
+}
 
-    struct SimpleImageInApp: Decodable {
-        let imageUrl: String
-        let redirectUrl: String
-        let intentPayload: String
-    }
-
-    enum InAppTargetingType: String, Decodable {
-        case simple
-    }
-
-    enum InAppFormType: String, Decodable {
-        case simpleImage
-    }
-
-    enum InAppFormPayload {
-        case simpleImage(SimpleImageInApp)
-    }
-
+// MARK: - InAppFormVariants
+extension InAppConfigResponse {
     struct InAppFormVariants: Decodable {
         let variants: [InAppForm]
     }
-
-    let inapps: [FailableDecodable<InApp>]
+    
+    struct InAppForm: Decodable {
+        let imageUrl: String
+        let redirectUrl: String
+        let intentPayload: String
+        let type: String
+        
+        enum CodingKeys: String, CodingKey {
+            case imageUrl
+            case redirectUrl
+            case intentPayload
+            case type = "$type"
+        }
+    }
 }
 
 struct FailableDecodable<T: Decodable>: Decodable {
