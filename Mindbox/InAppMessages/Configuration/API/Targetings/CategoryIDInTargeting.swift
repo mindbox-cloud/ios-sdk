24f56392b06d134775e73d12b6fd693ea2fc3d65
Akylbek Utekeshev <akylbek@workingeeks.com>
2023-03-21 03:18:27 +0600

    MBX-2365 Add CategoryIdIn to targeting model

---
+++ Mindbox/InAppMessages/Configuration/API/Targetings/CategoryIDInTargeting.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryIDInTargeting.swift
+//  Mindbox
+//
+//  Created by Akylbek Utekeshev on 21.03.2023.
+//  Copyright © 2023 Mindbox. All rights reserved.
+//
+
+import Foundation
+
+struct CategoryIDInTargeting: ITargeting, Decodable  {
+    let kind: CategoryKind
+    let values: String
+    
+    enum CategoryKind: String, Codable {
+        case any
+        case none
+    }
+    
+    struct CategoryIDInValue: Codable {
+        let id: String
+        let name: String
+        
+        enum CodingKeys: String, CodingKey {
+            case id = "externalId"
+            case name = "externalSystemName"
+        }
+    }
+}
