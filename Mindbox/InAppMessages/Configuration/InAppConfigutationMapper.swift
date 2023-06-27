daa9b383b86c5ea82282d43b597ca187b9ab6edf
Akylbek Utekeshev <utekeshev@mindbox.cloud>
2023-06-27 10:06:04 +0300

    MBX-Log fix

--- Mindbox/InAppMessages/Configuration/InAppConfigutationMapper.swift
+++ Mindbox/InAppMessages/Configuration/InAppConfigutationMapper.swift
@@ -312,7 +312,7 @@ final class InAppConfigutationMapper: InAppConfigurationMapperProtocol {
                                 return
                             }
                             
-                            Logger.common(message: "Image is loaded successfully. [ID]: \(inapp.inAppId)", level: .debug, category: .inAppMessages)
+                            Logger.common(message: "Image is loaded successfully. [URL]: \(inapp.imageUrl)", level: .debug, category: .inAppMessages)
                             formData = InAppFormData(inAppId: inapp.inAppId, image: image, redirectUrl: inapp.redirectUrl, intentPayload: inapp.intentPayload)
                             shouldDownloadImage = false
                         } catch {
