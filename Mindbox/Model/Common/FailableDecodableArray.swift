15d01e41f3d5bda848fff907c47f0d218fcaea70
Akylbek Utekeshev <utekeshev@mindbox.cloud>
2023-08-07 09:11:14 +0600

    MBX-2706 Added failable array

---
+++ Mindbox/Model/Common/FailableDecodableArray.swift
@@ -0,0 +1,35 @@
+//
+//  FailableDecodableArray.swift
+//  Mindbox
+//
+//  Created by vailence on 03.08.2023.
+//  Copyright © 2023 Mindbox. All rights reserved.
+//
+
+import Foundation
+
+struct FailableDecodableArray<Element: Decodable & Equatable>: Decodable, Equatable {
+
+    let elements: [Element]
+
+    init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+
+        var elements = [Element]()
+        if let count = container.count {
+            elements.reserveCapacity(count)
+        }
+
+        while !container.isAtEnd {
+            if let element = try? container.decode(Element.self) {
+                elements.append(element)
+            } else {
+                _ = try? container.decode(DummyCodable.self) // "Consumes" the failed element
+            }
+        }
+
+        self.elements = elements
+    }
+
+    private struct DummyCodable: Decodable { }
+}
