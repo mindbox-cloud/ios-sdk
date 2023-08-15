27334c1581ccf368282c36fa1791b5e31b99b57b
Akylbek Utekeshev <utekeshev@mindbox.cloud>
2023-08-15 17:54:06 +0600

    MBX-2707 UI

---
+++ Mindbox/InAppMessages/Presentation/Views/ViewFactory/SnackbarViewFactory.swift
@@ -0,0 +1,33 @@
+//
+//  SnackbarViewFactory.swift
+//  Mindbox
+//
+//  Created by vailence on 15.08.2023.
+//  Copyright © 2023 Mindbox. All rights reserved.
+//
+
+import UIKit
+
+//class SnackbarViewFactory: ViewFactoryProtocol {
+//    var myViewController: UIViewController?
+//
+//    func create(model: MindboxFormVariant,
+//                id: String,
+//                image: UIImage,
+//                onPresented: @escaping () -> Void,
+//                onTapAction: @escaping (ContentBackgroundLayerAction?) -> Void,
+//                onClose: @escaping () -> Void) -> UIViewController? {
+//        if case .snackbar(let snackbarFormVariant) = model {
+//            let viewController = ModalViewController(model: snackbarFormVariant,
+//                                                     id: id,
+//                                                     image: image,
+//                                                     onPresented: onPresented,
+//                                                     onTapAction: onTapAction,
+//                                                     onClose: onClose)
+//            myViewController = viewController
+//            return viewController
+//        }
+//
+//        return nil
+//    }
+//}
