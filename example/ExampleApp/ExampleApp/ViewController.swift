4d54fb64c8c8445a966f64a84e7af8a1faaaf481
justSmK <28645140+justSmK@users.noreply.github.com>
2024-03-11 12:04:26 +0300

    MBX-3198 ExampleApp init

---
+++ example/ExampleApp/ExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Sergei Semko on 3/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
