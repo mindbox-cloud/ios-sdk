3b8f6b0b4f3f71432ffeb4a7e4e147530d62d97a
justSmK <28645140+justSmK@users.noreply.github.com>
2024-03-11 20:00:12 +0300

    MBX-3198 Screen implementation with Device UUID

--- example/ExampleApp/ExampleApp/Application/AppDelegate.swift
+++ example/ExampleApp/ExampleApp/Application/AppDelegate.swift
@@ -9,12 +9,16 @@ import UIKit
 import Mindbox
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: MindboxAppDelegate {
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        // Call super when using MindboxAppDelegate
+        super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        registerForRemoteNotifications()
         
         do {
             let mindboxSdkConfiguration = try MBConfiguration(
@@ -25,30 +29,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             )
             
             Mindbox.shared.initialization(configuration: mindboxSdkConfiguration)
-            Mindbox.shared.getDeviceUUID { deviceUUID in
-                print(deviceUUID)
-            }
         } catch {
             print(error)
         }
         
         return true
     }
+    
+    private func registerForRemoteNotifications() {
+        UNUserNotificationCenter.current().delegate = self
+        DispatchQueue.main.async {
+            UIApplication.shared.registerForRemoteNotifications()
+            UNUserNotificationCenter.current()
+                .requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+                print("Permission granted \(granted)")
+                if let error {
+                    print("NotificationsRequestAuthorization failed with error: \(error.localizedDescription)")
+                }
+                Mindbox.shared.notificationsRequestAuthorization(granted: granted)
+            }
+        }
+    }
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        // TODO: Check `.list` instead `.alert`
+        completionHandler([.alert, .badge, .sound])
+    }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        return UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
+
 
--- example/ExampleApp/ExampleApp/ViewController.swift
+++ example/ExampleApp/ExampleApp/ViewController.swift
@@ -6,13 +6,83 @@
 //
 
 import UIKit
+import Mindbox
 
-class ViewController: UIViewController {
+fileprivate enum Constants {
+    static let copyButtonTitle = "Copy"
+    static let copyButtonSystemImageName = "doc.on.doc"
+}
+
+final class ViewController: UIViewController {
+    
+    private var deviceUUID = String()
+    
+    private lazy var deviceUuidLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var copyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(Constants.copyButtonTitle, for: .normal)
+        button.setImage(
+            UIImage(systemName: Constants.copyButtonSystemImageName),
+            for: .normal
+        )
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        setUpLayout()
+        setUpButton()
+        getDeviceUUID()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        showDeviceUUID()
+    }
 }
 
+private extension ViewController {
+    func setUpLayout() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(deviceUuidLabel)
+        view.addSubview(copyButton)
+        NSLayoutConstraint.activate([
+            deviceUuidLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            deviceUuidLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            deviceUuidLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            copyButton.topAnchor.constraint(equalTo: deviceUuidLabel.bottomAnchor, constant: 25),
+            copyButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            copyButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+    
+    func setUpButton() {
+        copyButton.addTarget(self, action: #selector(copyButtonDidTap), for: .touchUpInside)
+    }
+    
+    @objc
+    func copyButtonDidTap(_ sender: UIButton) {
+        let pastboard = UIPasteboard.general
+        pastboard.string = deviceUUID
+    }
+    
+    func showDeviceUUID() {
+        DispatchQueue.main.async {
+            self.deviceUuidLabel.text = self.deviceUUID
+        }
+    }
+    
+    func getDeviceUUID() {
+        Mindbox.shared.getDeviceUUID { deviceUUID in
+            self.deviceUUID = deviceUUID
+        }
+    }
+}
