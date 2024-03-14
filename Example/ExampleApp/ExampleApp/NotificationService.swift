20f9e91efd1f2ab94e0d517cd41d185170e86495
kniksees <dima500000studio@gmail.com>
2024-03-14 17:51:39 +0300

    MBX-3197: implemented rich-push

--- Example/ExampleApp/ExampleApp/NotificationService.swift
+++ Example/ExampleApp/ExampleApp/NotificationService.swift
@@ -7,20 +7,20 @@
 
 
 
-import UserNotifications
-import MindboxNotifications
-
-class NotificationService: UNNotificationServiceExtension {
-
-  lazy var mindboxService = MindboxNotificationService()
-
-  override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-    mindboxService.didReceive(request, withContentHandler: contentHandler)
-  }
-
-  override func serviceExtensionTimeWillExpire() {
-    // Called just before the extension will be terminated by the system.
-    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-    mindboxService.serviceExtensionTimeWillExpire()
-  }
-}
+//import UserNotifications
+//import MindboxNotifications
+//
+//class NotificationService: UNNotificationServiceExtension {
+//
+//  lazy var mindboxService = MindboxNotificationService()
+//
+//  override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+//    mindboxService.didReceive(request, withContentHandler: contentHandler)
+//  }
+//
+//  override func serviceExtensionTimeWillExpire() {
+//    // Called just before the extension will be terminated by the system.
+//    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+//    mindboxService.serviceExtensionTimeWillExpire()
+//  }
+//}
--- Example/ExampleApp/MindboxNotificationServiceExtension/NotificationService.swift
+++ Example/ExampleApp/MindboxNotificationServiceExtension/NotificationService.swift
@@ -6,30 +6,19 @@
 //
 
 import UserNotifications
+import MindboxNotifications
 
 class NotificationService: UNNotificationServiceExtension {
 
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
+  lazy var mindboxService = MindboxNotificationService()
 
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
-    }
-    
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
-    }
+  override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    mindboxService.didReceive(request, withContentHandler: contentHandler)
+  }
 
+  override func serviceExtensionTimeWillExpire() {
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    mindboxService.serviceExtensionTimeWillExpire()
+  }
 }
