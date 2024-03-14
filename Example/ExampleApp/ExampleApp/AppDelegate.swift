04e8045b4ffedef8949115f163d9415e0eca9c08
kniksees <dima500000studio@gmail.com>
2024-03-14 11:49:40 +0300

    MBX-3197: removed storyboard,  implemented push-notifications

--- Example/ExampleApp/ExampleApp/AppDelegate.swift
+++ Example/ExampleApp/ExampleApp/AppDelegate.swift
@@ -11,45 +11,83 @@ import Foundation
 
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, InAppMessagesDelegate {
+    func inAppMessageTapAction(id: String, url: URL?, payload: String) {
+        print("click")
+    }
+    
+    func inAppMessageDismissed(id: String) {
+        print("close")
+    }
+    
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         do {
-                    
+            
             let mindboxSdkConfig = try MBConfiguration(
                 endpoint: "Mpush-test.ExampleApp.IosApp",
-              domain: "api.mindbox.ru",
-              subscribeCustomerIfCreated: true,
-              shouldCreateCustomer: true
+                domain: "api.mindbox.ru",
+                subscribeCustomerIfCreated: true,
+                shouldCreateCustomer: true
             )
             
             Mindbox.shared.initialization(configuration: mindboxSdkConfig)
-            Mindbox.shared.getDeviceUUID{
+            Mindbox.shared.inAppMessagesDelegate = self
+            Mindbox.shared.getDeviceUUID {
                 deviceUUID in print(deviceUUID)
             }
         } catch  {
-           print(error)
+            print(error)
         }
-       
+        registerForRemoteNotifications()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
+    
+    func registerForRemoteNotifications() {
+        UNUserNotificationCenter.current().delegate = self
+        DispatchQueue.main.async {
+            UIApplication.shared.registerForRemoteNotifications()
+            UNUserNotificationCenter.current().requestAuthorization(options: [ .alert, .sound, .badge]) { granted, error in
+                print("Permission granted: \(granted)")
+                if let error = error {
+                    print("NotificationsRequestAuthorization failed with error: \(error.localizedDescription)")
+                }
+                Mindbox.shared.notificationsRequestAuthorization(granted: granted)
+            }
+        }
+    }
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+            // Остальной код в методе, если есть.
+            Mindbox.shared.apnsTokenUpdate(deviceToken: deviceToken)
+        }
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
+
+
 
--- Example/ExampleApp/ExampleApp/ViewController.swift
+++ Example/ExampleApp/ExampleApp/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import Mindbox
 
 class ViewController: UIViewController {
-
+    
+    let button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        button.setTitle("Show in-app", for: .normal)
+        view.addSubview(button)
+        button.backgroundColor = UIColor(red: 55/255, green: 169/255, blue: 92/255, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 200),
+            button.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
     }
-
-
+    
+    @objc private func didTapButton() {
+        let json = "{}"
+        Mindbox.shared.executeSyncOperation(operationSystemName: "Test1",
+                                            json: json) { result in
+            print("click")
+        }
+    }
 }
 
