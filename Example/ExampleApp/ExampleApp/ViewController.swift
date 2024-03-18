f3ca9ffd9104520bc2d2a87bfac8889381964030
kniksees <dima500000studio@gmail.com>
2024-03-18 15:06:42 +0300

    feature/MBX-3197: add buttons with executeAsyncOperation and executeSyncOperation

--- Example/ExampleApp/ExampleApp/ViewController.swift
+++ Example/ExampleApp/ExampleApp/ViewController.swift
@@ -10,23 +10,42 @@ import Mindbox
 
 class ViewController: UIViewController {
     
-    let button = UIButton()
+    let buttonAsync = UIButton()
+    let buttonSync = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        button.setTitle("Show in-app", for: .normal)
-        view.addSubview(button)
-        button.backgroundColor = UIColor(red: 55/255, green: 169/255, blue: 92/255, alpha: 1)
-        button.setTitleColor(.white, for: .normal)
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
+        buttonAsync.setTitle("Show in-app (with executeAsyncOperation)", for: .normal)
+        view.addSubview(buttonAsync)
+        buttonAsync.backgroundColor = UIColor(red: 55/255, green: 169/255, blue: 92/255, alpha: 1)
+        buttonAsync.setTitleColor(.white, for: .normal)
+        buttonAsync.addTarget(self, action: #selector(didTapButtonAsync), for: .touchUpInside)
+        buttonAsync.titleLabel?.numberOfLines = 0
+        buttonAsync.titleLabel?.lineBreakMode = .byWordWrapping
+        buttonAsync.translatesAutoresizingMaskIntoConstraints = false
+        
+        buttonSync.setTitle("Show in-app (with executeSyncOperation)", for: .normal)
+        view.addSubview(buttonSync)
+        buttonSync.backgroundColor = UIColor(red: 55/255, green: 169/255, blue: 92/255, alpha: 1)
+        buttonSync.setTitleColor(.white, for: .normal)
+        buttonSync.addTarget(self, action: #selector(didTapButtonAsync), for: .touchUpInside)
+        buttonSync.titleLabel?.numberOfLines = 0
+        buttonSync.titleLabel?.lineBreakMode = .byWordWrapping
+        buttonSync.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             
-            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            button.widthAnchor.constraint(equalToConstant: 200),
-            button.heightAnchor.constraint(equalToConstant: 100)
+            buttonAsync.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            //buttonAsync.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            buttonAsync.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
+            buttonAsync.widthAnchor.constraint(equalToConstant: 250),
+            buttonAsync.heightAnchor.constraint(equalToConstant: 100),
+            
+            buttonSync.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonSync.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 100),
+            buttonSync.widthAnchor.constraint(equalToConstant: 250),
+            buttonSync.heightAnchor.constraint(equalToConstant: 100),
         ])
     }
     
@@ -35,10 +54,22 @@ class ViewController: UIViewController {
     //Сreate an operation:
     //https://help.mindbox.ru/docs/%D0%BE%D0%BF%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D0%B8-v-%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D1%8B%D0%B5-%D1%81%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F
     //Change "operationSystemName" according to the name of the operation
-    @objc private func didTapButton() {
+    @objc private func didTapButtonAsync() {
         let json = "{}"
         Mindbox.shared.executeAsyncOperation(operationSystemName: "Test1",
                                              json: json)
+    }
+    
+    //Create an In-App according to these instructions:
+    //https://developers.mindbox.ru/docs/in-app-targeting-by-custom-operation
+    //Сreate an operation:
+    //https://help.mindbox.ru/docs/%D0%BE%D0%BF%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D0%B8-v-%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D1%8B%D0%B5-%D1%81%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F
+    //Change "operationSystemName" according to the name of the operation
+    @objc private func didTapButtonSync() {
+        let json = "{}"
+        Mindbox.shared.executeSyncOperation(operationSystemName: "Test1",
+                                            json: json) { result in
+        }
     }
 }
 
