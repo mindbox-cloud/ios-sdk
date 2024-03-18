6044f9ff45e67e1dc4c8b4a16f58dfe66678d42f
kniksees <dima500000studio@gmail.com>
2024-03-18 14:12:32 +0300

    feature/MBX-3197:  removed unnecessary files and functions,  added some instructions

--- Example/ExampleApp/ExampleApp/ViewController.swift
+++ Example/ExampleApp/ExampleApp/ViewController.swift
@@ -30,6 +30,11 @@ class ViewController: UIViewController {
         ])
     }
     
+    //Create an In-App according to these instructions:
+    //https://developers.mindbox.ru/docs/in-app-targeting-by-custom-operation
+    //Сreate an operation:
+    //https://help.mindbox.ru/docs/%D0%BE%D0%BF%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D0%B8-v-%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D1%8B%D0%B5-%D1%81%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F
+    //Change "operationSystemName" according to the name of the operation
     @objc private func didTapButton() {
         let json = "{}"
         Mindbox.shared.executeAsyncOperation(operationSystemName: "Test1",
