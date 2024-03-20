e91b16cbc514bdedc0979b9c4031c4ba42c6bfd1
kniksees <dima500000studio@gmail.com>
2024-03-20 15:55:19 +0300

    feature/MBX-3197: add comments

--- Example/ExampleApp/ExampleApp/AppDelegate.swift
+++ Example/ExampleApp/ExampleApp/AppDelegate.swift
@@ -13,6 +13,7 @@ import Foundation
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
+    //SDK initialization
     //https://developers.mindbox.ru/docs/ios-sdk-initialization
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         do {
@@ -33,7 +34,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         return true
     }
     
-    //https://developers.mindbox.ru/docs/ios-send-push-notifications-advanced
+    //Request permission to display notifications
+    //https://developers.mindbox.ru/docs/ios-send-push-notifications-advanced#2-%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81%D0%B8%D1%82%D1%8C-%D1%80%D0%B0%D0%B7%D1%80%D0%B5%D1%88%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D0%BE%D1%82%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D1%83%D0%B2%D0%B5%D0%B4%D0%BE%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D0%B9
     func registerForRemoteNotifications() {
         UNUserNotificationCenter.current().delegate = self
         DispatchQueue.main.async {
@@ -48,7 +50,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         }
     }
     
-    //https://developers.mindbox.ru/docs/ios-send-push-notifications-advanced
+    //Display standard notifications
+    //https://developers.mindbox.ru/docs/ios-send-push-notifications-advanced#3-%D1%80%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D0%BE%D1%82%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D1%81%D1%82%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D1%82%D0%BD%D1%8B%D1%85-%D1%83%D0%B2%D0%B5%D0%B4%D0%BE%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D0%B9
     func userNotificationCenter(
         _ center: UNUserNotificationCenter,
         willPresent notification: UNNotification,
@@ -57,6 +60,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         completionHandler([.alert, .badge, .sound])
     }
     
+    //Transferring clicks to push notifications
     //https://developers.mindbox.ru/docs/ios-get-click-advanced#1-%D0%BF%D0%B5%D1%80%D0%B5%D0%B4%D0%B0%D1%87%D0%B0-%D0%BA%D0%BB%D0%B8%D0%BA%D0%BE%D0%B2-%D0%BF%D0%BE-push-%D1%83%D0%B2%D0%B5%D0%B4%D0%BE%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F%D0%BC
     //https://developers.mindbox.ru/docs/ios-sdk-methods#pushclicked
     func userNotificationCenter(
@@ -67,6 +71,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             completionHandler()
         }
     
+    //Transfer to SDK APNs token
     //https://developers.mindbox.ru/docs/ios-send-push-notifications-advanced#4-%D0%BF%D0%B5%D1%80%D0%B5%D0%B4%D0%B0%D1%82%D1%8C-%D0%B2-sdk-apns-%D1%82%D0%BE%D0%BA%D0%B5%D0%BD
     //https://developers.mindbox.ru/docs/ios-sdk-methods#apnstokenupdate
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
