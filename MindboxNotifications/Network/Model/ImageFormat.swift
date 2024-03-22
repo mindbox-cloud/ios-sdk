e21e00b25c5eca36909795dc9d16a29cab5a7565
kniksees <dima500000studio@gmail.com>
2024-03-22 17:34:05 +0300

    feature/MBX-3233: add logs to ImageFormat

--- MindboxNotifications/Network/Model/ImageFormat.swift
+++ MindboxNotifications/Network/Model/ImageFormat.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import MindboxLogger
 
 enum ImageFormat: String {
     case png, jpg, gif
@@ -26,15 +27,22 @@ enum ImageFormat: String {
 
 extension ImageFormat {
     static func get(from data: Data) -> ImageFormat? {
-        guard let firstByte = data.first else { return nil }
+        guard let firstByte = data.first else { 
+            Logger.common(message: "ImageFormat: Failed to get firstByte. data \(data)", level: .error, category: .general)
+            return nil
+        }
         switch firstByte {
         case 0x89:
+            Logger.common(message: "ImageFormat: Successfully identified image format as .png", level: .info, category: .general)
             return .png
         case 0xFF:
+            Logger.common(message: "ImageFormat: Successfully identified image format as .jpg", level: .info, category: .general)
             return .jpg
         case 0x47:
+            Logger.common(message: "ImageFormat: Successfully identified image format as .gif", level: .info, category: .general)
             return .gif
         default:
+            Logger.common(message: "ImageFormat: Failed to get image format", level: .fault, category: .general)
             return nil
         }
     }
