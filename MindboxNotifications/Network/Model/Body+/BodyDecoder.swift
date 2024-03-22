105f6c90d76d3cb0021b86490c0c971cf746fc0f
kniksees <dima500000studio@gmail.com>
2024-03-22 11:33:42 +0300

    feature/MBX-3233:  add logs to BodyDecoder

--- MindboxNotifications/Network/Model/Body+/BodyDecoder.swift
+++ MindboxNotifications/Network/Model/Body+/BodyDecoder.swift
@@ -7,19 +7,24 @@
 //
 
 import Foundation
+import MindboxLogger
 
 struct BodyDecoder<T: Decodable> {
     
     let body: T
     
     init?(decodable: String) {
+        Logger.common(message: "BodyDecoder initialization", level: .info, category: .notification)
         if let data = decodable.data(using: .utf8) {
             if let body = try? JSONDecoder().decode(T.self, from: data) {
+                Logger.common(message: "Body decoding successful", level: .info, category: .notification)
                 self.body = body
             } else {
+                Logger.common(message: "JSON decoding returned nil, Data: \(data)", level: .fault, category: .notification)
                 return nil
             }
         } else {
+            Logger.common(message: "String decoding returned nil. Data: \(decodable)", level: .fault, category: .notification)
             return nil
         }
     }
