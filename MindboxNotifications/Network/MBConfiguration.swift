2d0d3fb73a3a61ba7ad2f76ccfdec70a2f4784a6
kniksees <dima500000studio@gmail.com>
2024-03-25 12:03:00 +0300

    feature/MBX-3233: add logs to MBConfiguration

--- MindboxNotifications/Network/MBConfiguration.swift
+++ MindboxNotifications/Network/MBConfiguration.swift
@@ -8,6 +8,7 @@
 
 
 import Foundation
+import MindboxLogger
 
 public struct MBConfiguration: Codable {
     public let endpoint: String
@@ -23,16 +24,28 @@ public struct MBConfiguration: Codable {
         for bundle in Bundle.allBundles {
             if let url = bundle.url(forResource: plistName, withExtension: "plist") {
                 findeURL = url
+                Logger.common(message: "MBConfiguration: Successfully finded URL. findeURL: \(String(describing: findeURL))", level: .info, category: .notification)
                 break
             }
         }
 
-        guard let url = findeURL else { return nil }
+        guard let url = findeURL else {
+            Logger.common(message: "MBConfiguration: Failed to find URL: findeURL: \(String(describing: findeURL))", level: .error, category: .notification)
+            return nil
+        }
 
-        guard let data = try? Data(contentsOf: url) else { return nil }
+        guard let data = try? Data(contentsOf: url) else {
+            Logger.common(message: "MBConfiguration: Failed to get data by URL. url: \(url)", level: .error, category: .notification)
+            return nil
+        }
 
-        guard let configuration = try? decoder.decode(MBConfiguration.self, from: data) else { return nil }
+        guard let configuration = try? decoder.decode(MBConfiguration.self, from: data) else {
+            Logger.common(message: "MBConfiguration: Failed to decode configuration from data. data: \(data)", level: .error, category: .notification)
+            return nil
+        }
+
         self = configuration
+        Logger.common(message: "LegacyFormatStrategy: Successfully initialized configuration. configuration. \(configuration)", level: .info, category: .notification)
     }
 
     enum CodingKeys: String, CodingKey {
@@ -45,27 +58,34 @@ public struct MBConfiguration: Codable {
 
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
+        Logger.common(message: "MBConfiguration: Successfully received values. values: \(values)", level: .info, category: .notification)
         let endpoint = try values.decode(String.self, forKey: .endpoint)
+        Logger.common(message: "MBConfiguration: Successfully received endpoint. endpoint: \(endpoint)", level: .info, category: .notification)
         let domain = try values.decode(String.self, forKey: .domain)
+        Logger.common(message: "MBConfiguration: Successfully received domain. domain: \(endpoint)", level: .info, category: .notification)
         var previousInstallationId: String?
         if let value = try? values.decode(String.self, forKey: .previousInstallationId) {
+            Logger.common(message: "MBConfiguration: Successfully decoded value by values for key 'previousInstallationId'. value: \(value)", level: .info, category: .notification)
             if !value.isEmpty {
                 previousInstallationId = value
             }
         }
         var previousDeviceUUID: String?
         if let value = try? values.decode(String.self, forKey: .previousDeviceUUID) {
+            Logger.common(message: "MBConfiguration: Successfully decoded value by values for key 'previousDeviceUUID'. value: \(value)", level: .info, category: .notification)
             if !value.isEmpty {
                 previousDeviceUUID = value
             }
         }
         let subscribeCustomerIfCreated = try values.decodeIfPresent(Bool.self, forKey: .subscribeCustomerIfCreated) ?? false
+        Logger.common(message: "MBConfiguration: subscribeCustomerIfCreated: \(subscribeCustomerIfCreated)", level: .info, category: .notification)
         
         self.endpoint = endpoint
         self.domain = domain
         self.previousDeviceUUID = previousDeviceUUID
         self.previousInstallationId = previousInstallationId
         self.subscribeCustomerIfCreated = subscribeCustomerIfCreated
+        Logger.common(message: "LegacyFormatStrategy: Successfully initialized configuration. configuration", level: .info, category: .notification)
     }
 }
 
