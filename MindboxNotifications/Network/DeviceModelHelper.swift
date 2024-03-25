d28cc1c1c4f9e654a399c007e244c733b7b10c8a
kniksees <dima500000studio@gmail.com>
2024-03-25 12:01:51 +0300

    feature/MBX-3233: update logs in DeviceModelHelper

--- MindboxNotifications/Network/DeviceModelHelper.swift
+++ MindboxNotifications/Network/DeviceModelHelper.swift
@@ -21,10 +21,10 @@ struct DeviceModelHelper {
         let machineMirror = Mirror(reflecting: systemInfo.machine)
         let identifier = machineMirror.children.reduce("") { identifier, element in
             guard let value = element.value as? Int8, value != 0 else {
-                Logger.common(message: "DeviceModelHelper: failed to cast element.value into Int8 or value == 0. element.value: \(element.value), value: \(0), identifier: \(identifier)", level: .error, category: .general)
+                Logger.common(message: "DeviceModelHelper: failed to cast element.value into Int8 or value == 0. element.value: \(element.value), value: \(0), identifier: \(identifier)", level: .error, category: .notification)
                 return identifier
             }
-            Logger.common(message: "DeviceModelHelper: Successfully created identifier. Identifier: \(identifier)", level: .info, category: .general)
+            Logger.common(message: "DeviceModelHelper: Successfully created identifier. Identifier: \(identifier)", level: .info, category: .notification)
             return identifier + String(UnicodeScalar(UInt8(value)))
         }
         return identifier
