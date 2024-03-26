3b7f3df945706b6fbd33cf52483e553fe1b5076e
kniksees <dima500000studio@gmail.com>
2024-03-26 13:57:34 +0300

    feature/MBX-3233: update logs in NotificationDecoder

--- MindboxNotifications/Network/Model/NotificationDecoder.swift
+++ MindboxNotifications/Network/Model/NotificationDecoder.swift
@@ -14,16 +14,15 @@ struct NotificationDecoder<T: Codable> {
     var isMindboxNotification: Bool {
         userInfo["uniqueKey"] != nil
     }
-    
+
     private let userInfo: [AnyHashable: Any]
-    
+
     init?(request: UNNotificationRequest) {
         guard let userInfo = (request.content.mutableCopy() as? UNMutableNotificationContent)?.userInfo else {
             Logger.common(message: "NotificationDecoder: Failed to get user info from notification content", level: .fault, category: .notification)
             return nil
         }
         
-        Logger.common(message: "NotificationDecoder: Successfully initialized NotificationDecoder. UserInfo \(userInfo)", level: .info, category: .notification)
         self.init(userInfo: userInfo)
     }
     
@@ -42,18 +41,16 @@ struct NotificationDecoder<T: Codable> {
     func decode() throws -> T {
         do {
             let data = try JSONSerialization.data(withJSONObject: userInfo, options: .prettyPrinted)
-            Logger.common(message: "NotificationDecoder: Successfully serialized userInfo into data. data: \(data)", level: .info, category: .notification)
             let decoder = JSONDecoder()
             do {
                 let payload = try decoder.decode(T.self, from: data)
-                Logger.common(message: "NotificationDecoder: Successfully decoded data into payload. payload: \(payload)", level: .info, category: .notification)
                 return payload
             } catch {
-                Logger.common(message: "NotificationDecoder: Failed to decode data into payloadpayload. error: \(error)", level: .error, category: .notification)
+                Logger.common(message: "NotificationDecoder: Failed to decode data into payload. error: \(error)", level: .error, category: .notification)
                 throw error
             }
         } catch {
-            Logger.common(message: "NotificationDecoder: Failed to serialize userInfo into data. error: \(error)", level: .error, category: .general)
+            Logger.common(message: "NotificationDecoder: Failed to serialize userInfo into data. error: \(error)", level: .error, category: .notification)
             throw error
         }
     }
