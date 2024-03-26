b9570cb421f2281941e5859513ca91145506c12d
justSmK <28645140+justSmK@users.noreply.github.com>
2024-03-26 16:15:08 +0300

    MBX-3100 Rewrite testDateTimeOffset with MockEvent

--- MindboxTests/GuaranteedDelivery/GuaranteedDeliveryTestCase.swift
+++ MindboxTests/GuaranteedDelivery/GuaranteedDeliveryTestCase.swift
@@ -80,48 +80,12 @@ class GuaranteedDeliveryTestCase: XCTestCase {
     }
 
     func testDateTimeOffset() {
-        let events = eventGenerator.generateEvents(count: 100)
-        do {
-            try events.forEach {
-
-                try databaseRepository.create(event: $0)
-                
-                guard let createdCdEvent = try databaseRepository.read(by: $0.transactionId) else {
-                    XCTFail("Event must be exist")
-                    return
-                }
-                
-                createdCdEvent.retryTimestamp = Date().timeIntervalSince1970 + 1000
-                
-                guard let eventToUpdate = Event(createdCdEvent) else {
-                    XCTFail("Failed to initialize Event with CdEvent")
-                    return
-                }
-                
-                try databaseRepository.update(event: eventToUpdate)
-                
-                guard let updatedEvent = try databaseRepository.read(by: $0.transactionId) else {
-                    XCTFail("UpdatedEvent must be exist")
-                    return
-                }
-                
-                guard let eventToTest = Event(updatedEvent) else {
-                    XCTFail("Failed to initialize Event")
-                    return
-                }
-                
-                XCTAssertTrue(eventToTest.isRetry, "isRetry must be true")
-                
-                let enqueueDate = Date(timeIntervalSince1970: eventToTest.enqueueTimeStamp)
-                let expectation = Int64((Date().timeIntervalSince(enqueueDate) * 1000).rounded())
-                let dateTimeOffset = eventToTest.dateTimeOffset
-                
-                let tolerance: Int64 = 100
-
-                XCTAssertTrue(abs(expectation - dateTimeOffset) <= tolerance)
-            }
-        } catch {
-            XCTFail(error.localizedDescription)
+        let events = eventGenerator.generateMockEvents(count: 100)
+        events.forEach { event in
+            let enqueueDate = Date(timeIntervalSince1970: event.enqueueTimeStamp)
+            let expectation = Int64((Date().timeIntervalSince(enqueueDate) * 1000).rounded())
+            let dateTimeOffset = event.dateTimeOffset
+            XCTAssertTrue(expectation == dateTimeOffset)
         }
     }
 
---
+++ MindboxTests/Mock/MockEvent.swift
@@ -0,0 +1,72 @@
+//
+//  MockEvent.swift
+//  MindboxTests
+//
+//  Created by Sergei Semko on 3/26/24.
+//  Copyright © 2024 Mindbox. All rights reserved.
+//
+
+import Foundation
+@testable import Mindbox
+
+struct MockEvent: EventProtocol {
+    let transactionId: String
+    
+    var dateTimeOffset: Int64 {
+        guard isRetry else {
+            return 0
+        }
+        let enqueueDate = Date(timeIntervalSince1970: enqueueTimeStamp)
+        let ms = (Date().timeIntervalSince(enqueueDate) * 1000).rounded()
+        return Int64(ms)
+    }
+    
+    let enqueueTimeStamp: Double
+    
+    let serialNumber: String?
+    
+    let type: Event.Operation
+    
+    let isRetry: Bool
+    
+    let body: String
+    
+    init(type: Event.Operation, body: String) {
+        self.transactionId = UUID().uuidString
+        self.enqueueTimeStamp = Date().timeIntervalSince1970
+        self.type = type
+        self.body = body
+        self.serialNumber = nil
+        self.isRetry = false
+    }
+    
+    init?(_ event: CDEvent) {
+        guard let transactionId = event.transactionId else {
+            return nil
+        }
+        guard let type = event.type, let operation = Event.Operation(rawValue: type) else {
+            return nil
+        }
+        guard let body = event.body else {
+
+            return nil
+        }
+        self.transactionId = transactionId
+        self.enqueueTimeStamp = event.timestamp
+        self.type = operation
+        self.body = body
+        self.serialNumber = event.objectID.uriRepresentation().lastPathComponent
+        self.isRetry = !event.retryTimestamp.isZero
+    }
+}
+
+extension MockEvent {
+    init(type: Event.Operation, body: String, isRetry: Bool) {
+        self.transactionId = UUID().uuidString
+        self.enqueueTimeStamp = Date().timeIntervalSince1970
+        self.type = type
+        self.body = body
+        self.serialNumber = nil
+        self.isRetry = isRetry
+    }
+}
