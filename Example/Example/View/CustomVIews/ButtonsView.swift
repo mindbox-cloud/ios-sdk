4fcc17077432d3496a6a64552cac12b1468fe309
Dmitry Erofeev <56806136+kniksees@users.noreply.github.com>
2024-04-04 15:34:51 +0300

    MBX-3248: Example (#323)
    * feature/MBX-3248-ExampleApp: add ExampleApp
    
    * feature/MBX-3248-ExampleApp: change MainViewModel init
    
    * feature/MBX-3248-ExampleApp: code formatting
    
    * feature/MBX-3248-ExampleApp: add rich-push
    
    * feature/MBX-3248-ExampleApp:  add MindboxNotificationContentExtension
    
    * feature/MBX-3248-ExampleApp: add InAppMessagesDelegate
    
    * feature/MBX-3248-ExampleApp: UI changing
    
    * feature/MBX-3248-ExampleApp: add ChooseInappMessageDelegate enum
    
    * feature/MBX-3248-ExampleApp: add comments
    
    * feature/MBX-3248-ExampleApp: code formatting
    
    * feature/MBX-3248-ExampleApp: Create README.md
    
    * feature/MBX-3248-ExampleApp: Update README.md
    
    * feature/MBX-3248-ExampleApp: Update README.md
    
    * feature/MBX-3248-ExampleApp: Update README.md
    
    * feature/MBX-3248-ExampleApp: Update README.md
    
    * feature/MBX-3248-ExampleApp: Update README.md
    
    * feature/MBX-3248-ExampleApp: Update README.md
    
    * feature/MBX-3248-ExampleApp: remove folder
    
    * feature/MBX-3248-ExampleApp: add ChooseInappMessageDelegate
    
    * feature/MBX-3248-ExampleApp: update Podfile
    
    * feature/MBX-3248-ExampleApp: update viewModel
    
    * feature/MBX-3248-ExampleApp: change icon
    
    * feature/MBX-3248-ExampleApp: update LounchScreen
    
    * feature/MBX-3248-ExampleApp: code alignment
    
    * feature/MBX-3248-ExampleApp: update launchscreen
    
    * feature/MBX-3248-ExampleApp: update launchscreen
    
    * feature/MBX-3248-ExampleApp: update launchscreen
    
    * feature/MBX-3248-ExampleApp: update SceneDelegate
    
    * feature/MBX-3248-ExampleApp: move ChooseInAppMessagesDelegate from AppDelegate to ViewModel
    
    * feature/MBX-3248-ExampleApp: Update README.md
    
    * feature/MBX-3248-ExampleApp: remove extra launch screen
    
    * feature/MBX-3248-ExampleApp: Update README.md
    
    * feature/MBX-3248-ExampleApp: Update README.md
    
    * feature/MBX-3248-ExampleApp: Update README.md
    
    * feature/MBX-3248-ExampleApp: Update README.md

---
+++ Example/Example/View/CustomVIews/ButtonsView.swift
@@ -0,0 +1,54 @@
+//
+//  ButtonsView.swift
+//  Example
+//
+//  Created by Дмитрий Ерофеев on 29.03.2024.
+//
+
+import SwiftUI
+
+struct ButtonsViewline: View {
+    
+    var label: String
+    var action: () -> ()
+    
+    var body: some View {
+        HStack {
+            Text(label)
+                .foregroundStyle(.mbText)
+            Spacer()
+            Button("Show In-App") {
+                action()
+            }
+            .frame(width: 120)
+            .frame(height: 30)
+            .background(Color.mbGreen)
+            .cornerRadius(10)
+            .tint(.white)
+        }
+    }
+}
+
+struct ButtonsView: View {
+    
+    @ObservedObject var viewModel: MainViewModel
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 350)
+                .frame(height: 110)
+                .foregroundColor(.mbView)
+            VStack {
+                ButtonsViewline(label: "Async operation") {
+                    viewModel.showInAppWithExecuteAsyncOperation()
+                }
+                Divider()
+                ButtonsViewline(label: "Sync operation") {
+                    viewModel.showInAppWithExecuteSyncOperation()
+                }
+            }
+            .frame(width: 310)
+        }
+    }
+}
---
+++ Example/Example/ViewModel/MainViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  MainViewModel.swift
+//  Example
+//
+//  Created by Дмитрий Ерофеев on 29.03.2024.
+//
+
+import Foundation
+import Mindbox
+import UIKit
+
+class MainViewModel: ObservableObject {
+    
+    @Published var SDKVersion: String = ""
+    @Published var deviceUUID: String = ""
+    @Published var APNSToken: String = ""
+    
+    //https://developers.mindbox.ru/docs/ios-sdk-methods
+    func setupData() {
+        self.SDKVersion = Mindbox.shared.sdkVersion
+        Mindbox.shared.getDeviceUUID { deviceUUID in
+            DispatchQueue.main.async {
+                self.deviceUUID = deviceUUID
+            }
+        }
+        Mindbox.shared.getAPNSToken { APNSToken in
+            DispatchQueue.main.async {
+                self.APNSToken = APNSToken
+            }
+        }
+        ChooseInAppMessagesDelegate.shared.select(chooseInappMessageDelegate: .InAppMessagesDelegate)
+    }
+    
+    //https://developers.mindbox.ru/docs/in-app-targeting-by-custom-operation
+    func showInAppWithExecuteSyncOperation () {
+        let json = """
+        { "viewProduct":
+            { "product":
+                { "ids":
+                    { "website": "1" }
+                }
+            }
+        }
+        """
+        Mindbox.shared.executeSyncOperation(operationSystemName: "APIMethodForReleaseExampleIos", json: json) { result in
+            switch result {
+            case .success(let success):
+                Mindbox.logger.log(level: .info, message: "\(success)")
+            case .failure(let error):
+                Mindbox.logger.log(level: .error, message: "\(error)")
+            }
+        }
+    }
+    
+    //https://developers.mindbox.ru/docs/in-app-targeting-by-custom-operation
+    func showInAppWithExecuteAsyncOperation () {
+        let json = """
+        { "viewProduct":
+            { "product":
+                { "ids":
+                    { "website": "2" }
+                }
+            }
+        }
+        """
+        Mindbox.shared.executeAsyncOperation(operationSystemName: "APIMethodForReleaseExampleIos", json: json)
+    }
+}
+
+
+
+
