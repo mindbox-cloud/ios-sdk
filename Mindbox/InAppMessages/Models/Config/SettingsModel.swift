dbb7186acff511b9686c1cc610a6cf3b47c34616
Akylbek Utekeshev <utekeshev@mindbox.cloud>
2024-04-11 01:41:34 +0500

    MBX-3252 [WIP] Added Frequency to Config

--- Mindbox/InAppMessages/Models/Config/SettingsModel.swift
+++ Mindbox/InAppMessages/Models/Config/SettingsModel.swift
@@ -22,3 +22,37 @@ struct Settings: Decodable, Equatable {
         }
     }
 }
+
+extension Settings.TimeToLive {
+    struct TTLUnit: Decodable, Equatable {
+        let unit: Unit?
+        let value: Int?
+        
+        init(unit: Unit?, value: Int?) {
+            self.unit = unit
+            self.value = value
+        }
+        
+        private enum CodingKeys: String, CodingKey {
+            case unit, value
+        }
+    }
+}
+
+enum Unit: String, Decodable {
+    case seconds = "seconds"
+    case minutes = "minutes"
+    case hours = "hours"
+    case days = "days"
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let decodedString = try container.decode(String.self).lowercased() // Преобразование к нижнему регистру
+        
+        guard let value = Unit(rawValue: decodedString) else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Неизвестная единица времени")
+        }
+        
+        self = value
+    }
+}
