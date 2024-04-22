db99904649082e9207770fc46c3b665ebd777829
justSmK <28645140+justSmK@users.noreply.github.com>
2024-04-22 19:13:56 +0300

    MBX-3332: Fix output bash

--- git-release-branch-create.sh
+++ git-release-branch-create.sh
@@ -36,40 +36,40 @@ sdkversionconfig_file="SDKVersionProvider/SDKVersionConfig.xcconfig"
 current_version=$(grep -E '^\s+spec.version\s+=' "$podspec_file" | cut -d'"' -f2)
 
 # Обновление версии в Mindbox.podspec
-echo "Before updating version:"
-grep "Mindbox spec.version" $podspec_file
+echo "Mindbox.podspec Before updating version:"
+grep "spec.version" $podspec_file
 
 sed -i '' "s/^[[:space:]]*spec.version[[:space:]]*=[[:space:]]*\".*\"$/  spec.version      = \"$version\"/" $podspec_file
 
-echo "After updating version:"
-grep "Mindbox spec.version" $podspec_file
+echo "Mindbox.podspec After updating version:"
+grep "spec.version" $podspec_file
 
 # Обновление версии в MindboxNotifications.podspec
-echo "Before updating version:"
-grep "MindboxNotifications spec.version" $notifivation_podspec_file
+echo "MindboxNotifications.podspec Before updating version:"
+grep "spec.version" $notifivation_podspec_file
 
 sed -i '' "s/^[[:space:]]*spec.version[[:space:]]*=[[:space:]]*\".*\"$/  spec.version      = \"$version\"/" $notifivation_podspec_file
 
-echo "After updating version:"
-grep "MindboxNotifications spec.version" $notifivation_podspec_file
+echo "MindboxNotifications.podspec After updating version:"
+grep "spec.version" $notifivation_podspec_file
 
 # Обновление версии в SDKVersionProvider.swift
-echo "Before updating version:"
-grep "SDKVersionProvider.swift spec.version" $sdkversionprovider_file
+echo "SDKVersionProvider.swift Before updating version:"
+grep "spec.version" $sdkversionprovider_file
 
 sed -i '' "s/public static let sdkVersion = \".*\"$/public static let sdkVersion = \"$version\"/" $sdkversionprovider_file
 
-echo "After updating version:"
-grep "SDKVersionConfig.xcconfig spec.version" $sdkversionprovider_file
+echo "SDKVersionProvider.swift After updating version:"
+grep "spec.version" $sdkversionprovider_file
 
 # Обновление MARKETING_VERSION в SDKVersionConfig.xcconfig
-echo "Before updating version:"
-grep "SDKVersionProvider.swift spec.version" $sdkversionconfig_file
+echo "SDKVersionConfig.xcconfig Before updating version:"
+grep "spec.version" $sdkversionconfig_file
 
 sed -i '' "s/^MARKETING_VERSION = .*$/MARKETING_VERSION = $version/" $sdkversionconfig_file
 
-echo "After updating version:"
-grep "SDKVersionConfig.xcconfig spec.version" $sdkversionconfig_file
+echo "SDKVersionConfig.xcconfig After updating version:"
+grep "spec.version" $sdkversionconfig_file
 
 echo "Bump SDK version from $current_version to $version."
 
