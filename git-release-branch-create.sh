4a79003a927b3df7f79ae84f1812ea27f31a80a2
Sergei Semko <28645140+justSmK@users.noreply.github.com>
2024-04-24 14:20:14 +0300

    MBX-3332: Trigger creation of release branch by creating branch (#374)
    * MBX-3332: Temporary update build SDK CI
    
    * MBX-3332: Update git-release-branch-create.sh
    
    * MBX-3332: Update .github/workflows/prepare_release_branch.yml
    
    * MBX-3332: Update gitignore and delete
    
    * MBX-3332: Refactoring
    
    * MBX-3332: Rewrite scripts to release format
    
    * MBX-3332: Update `Create pull request` action
    Remove `draft` flag
    Change base branch from develop to master
    
    * MBX-3332: Remove develop branch check

--- git-release-branch-create.sh
+++ git-release-branch-create.sh
@@ -7,54 +7,99 @@ exit 1
 fi
 
 #Check if the version number matches the semver format
-if ! [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+(-rc)?$ ]]; then
+if ! [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+(-rc)?$ ]]; then
 echo "The release version number does not match the semver format (X.Y.Z or X.Y.Z-rc)."
 exit 1
 fi
 
 #Check the current Git branch
 current_branch=$(git symbolic-ref --short HEAD)
+echo "Currently on branch: $current_branch"
 
-if [[ $current_branch != "develop" && ! $current_branch =~ ^release/[0-9]+.[0-9]+.[0-9]+(-rc)?$ ]]; then
-echo "The current Git branch ($current_branch) is not 'develop' or in the format 'release/X.Y.Z' or 'release/X.Y.Z-rc'."
+if [[ ! $current_branch =~ ^release/[0-9]+\.[0-9]+\.[0-9]+(-rc)?$ ]]; then
+echo "The current Git branch ($current_branch) is not in the format 'release/X.Y.Z' or 'release/X.Y.Z-rc'."
 exit 1
 fi
 
-#Create a branch with the version name
 version=$1
-branch_name="release/$version"
-git branch $branch_name
-git checkout $branch_name
 
 #Add changelog to the index and create a commit
 podspec_file="Mindbox.podspec"
-notifivation_podspec_file="MindboxNotifications.podspec"
+notification_podspec_file="MindboxNotifications.podspec"
 sdkversionprovider_file="SDKVersionProvider/SDKVersionProvider.swift"
 sdkversionconfig_file="SDKVersionProvider/SDKVersionConfig.xcconfig"
+
 current_version=$(grep -E '^\s+spec.version\s+=' "$podspec_file" | cut -d'"' -f2)
 
-# Обновление версии в Mindbox.podspec
-sed -i '' "s/^[[:space:]]*spec.version[[:space:]]*=[[:space:]]*\".*\"$/  spec.version      = \"$version\"/" $podspec_file
+# Update Mindbox.podspec version
+echo "`Mindbox.podspec` Before updating version:"
+grep "spec.version" $podspec_file
 
-# Обновление версии в MindboxNotifications.podspec
-sed -i '' "s/^[[:space:]]*spec.version[[:space:]]*=[[:space:]]*\".*\"$/  spec.version      = \"$version\"/" $notifivation_podspec_file
+sed -i '' "s/^\([[:space:]]*spec.version[[:space:]]*=[[:space:]]*\"\).*\(\"$\)/\1$version\2/" $podspec_file
 
-# Обновление версии в SDKVersionProvider.swift
-sed -i '' "s/public static let sdkVersion = \".*\"$/public static let sdkVersion = \"$version\"/" $sdkversionprovider_file
+echo "`Mindbox.podspec` After updating version:"
+grep "spec.version" $podspec_file
 
-# Обновление MARKETING_VERSION в SDKVersionConfig.xcconfig
-sed -i '' "s/^MARKETING_VERSION = .*$/MARKETING_VERSION = $version/" $sdkversionconfig_file
+# Update MindboxNotifications.podspec version
+echo "`MindboxNotifications.podspec` Before updating version:"
+grep "spec.version" $notification_podspec_file
+
+sed -i '' "s/^\([[:space:]]*spec.version[[:space:]]*=[[:space:]]*\"\).*\(\"$\)/\1$version\2/" $notification_podspec_file
+
+echo "`MindboxNotifications.podspec` After updating version:"
+grep "spec.version" $notification_podspec_file
+
+# Update SDKVersionProvider.swift version
+echo "`SDKVersionProvider.swift` Before updating version:"
+grep "sdkVersion" $sdkversionprovider_file
+
+sed -i '' "s/\(public static let sdkVersion = \"\).*\(\"$\)/\1$version\2/" $sdkversionprovider_file
+
+echo "`SDKVersionProvider.swift` After updating version:"
+grep "sdkVersion" $sdkversionprovider_file
+
+# Update `SDKVersionConfig.xcconfig` version
+echo "`SDKVersionConfig.xcconfig` Before updating version:"
+grep "MARKETING_VERSION" $sdkversionconfig_file
+
+sed -i '' "s/\(MARKETING_VERSION = \).*\$/\1$version/" $sdkversionconfig_file
+
+echo "`SDKVersionConfig.xcconfig` After updating version:"
+grep "MARKETING_VERSION" $sdkversionconfig_file
 
 echo "Bump SDK version from $current_version to $version."
 
 git add $podspec_file
-git add $notifivation_podspec_file
+git add $notification_podspec_file
 git add $sdkversionprovider_file
-git commit -m "Bump SDK version to $version"
+git add $sdkversionconfig_file
+git commit -m "Bump SDK version from $current_version to $version"
 
-git push origin $branch_name
 
-git tag $branch_name
-git push origin $branch_name --tags
+# Update `Example/Podfile` version
+podfile="Example/Podfile"
 
-echo "Branch $branch_name has been created and pushed."
+echo "`Example Podfile` Before updating version:"
+grep "pod 'Mindbox'" $podfile
+grep "pod 'MindboxNotifications'" $podfile
+
+sed -i '' "s/\(pod 'Mindbox', '\)[^']*\(\'\)/\1$version\2/g" $podfile
+sed -i '' "s/\(pod 'MindboxNotifications', '\)[^']*\(\'\)/\1$version\2/g" $podfile
+
+echo "`Example Podfile After` updating version:"
+grep "pod 'Mindbox'" $podfile
+grep "pod 'MindboxNotifications'" $podfile
+
+git add $podfile
+git commit -m "Bump `Example/Podfile` SDK version from $current_version to $version"
+
+echo "Pushing changes to branch: $current_branch"
+if ! git push origin $current_branch; then
+    echo "Failed to push changes to the origin $current_branch"
+    exit 1
+fi
+
+git tag $version
+git push origin $version
+
+echo "Changes have been committed and tagged as $version on branch $current_branch."
