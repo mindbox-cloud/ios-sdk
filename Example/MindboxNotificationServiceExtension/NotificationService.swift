79ec1df1c04108b5ba2c79894506d0c5aae4d224
justSmK <28645140+justSmK@users.noreply.github.com>
2024-06-11 19:34:27 +0300

    Fix NotificationService

--- Example/MindboxNotificationServiceExtension/NotificationService.swift
+++ Example/MindboxNotificationServiceExtension/NotificationService.swift
@@ -14,20 +14,6 @@ class NotificationService: UNNotificationServiceExtension {
     
     lazy var mindboxService = MindboxNotificationService()
     
-    @MainActor
-    private func saveSwiftDataItem(_ pushNotificatoin: MBPushNotification) async {
-        let context = SwiftDataManager.shared.container.mainContext
-
-        let newItem = Item(timestamp: Date(), pushNotification: pushNotificatoin)
-        
-        context.insert(newItem)
-        do {
-            try context.save()
-        } catch {
-            print("Failed to save context: \(error.localizedDescription)")
-        }
-    }
-    
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         if let mindboxPushNotification = Mindbox.shared.getMindboxPushData(userInfo: request.content.userInfo) {
             Task {
@@ -42,5 +28,19 @@ class NotificationService: UNNotificationServiceExtension {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
         mindboxService.serviceExtensionTimeWillExpire()
+    }
+    
+    @MainActor
+    private func saveSwiftDataItem(_ pushNotification: MBPushNotification) async {
+        let context = SwiftDataManager.shared.container.mainContext
+
+        let newItem = Item(timestamp: Date(), pushNotification: pushNotification)
+        
+        context.insert(newItem)
+        do {
+            try context.save()
+        } catch {
+            print("Failed to save context: \(error.localizedDescription)")
+        }
     }
 }
