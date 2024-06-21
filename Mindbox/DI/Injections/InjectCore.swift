bbd11ef0acde0ac007d3aea9195c46528c2f4d8a
Akylbek Utekeshev <utekeshev@mindbox.cloud>
2024-06-21 19:05:52 +0400

    MBX-0000 Comment unused stuff

--- Mindbox/DI/Injections/InjectCore.swift
+++ Mindbox/DI/Injections/InjectCore.swift
@@ -9,22 +9,22 @@
 import Foundation
 
 extension MBContainer {
-    func registerCore() -> Self {
-        register(CoreController.self, scope: .container) {
-            let instanceFactory = container.injectOrFail(InstanceFactory.self)
-            return CoreController(
-                persistenceStorage: container.injectOrFail(PersistenceStorage.self),
-                utilitiesFetcher: container.injectOrFail(UtilitiesFetcher.self),
-                notificationStatusProvider: container.injectOrFail(UNAuthorizationStatusProviding.self),
-                databaseRepository: container.injectOrFail(MBDatabaseRepository.self),
-                guaranteedDeliveryManager: container.injectOrFail(GuaranteedDeliveryManager.self),
-                trackVisitManager: instanceFactory.makeTrackVisitManager(),
-                sessionManager: container.injectOrFail(MBSessionManager.self),
-                inAppMessagesManager: container.injectOrFail(InAppCoreManagerProtocol.self),
-                uuidDebugService: container.injectOrFail(UUIDDebugService.self),
-                userVisitManager: container.injectOrFail(UserVisitManagerProtocol.self))
-        }
-        
-        return self
-    }
+//    func registerCore() -> Self {
+//        register(CoreController.self, scope: .container) {
+//            let instanceFactory = container.injectOrFail(InstanceFactory.self)
+//            return CoreController(
+//                persistenceStorage: container.injectOrFail(PersistenceStorage.self),
+//                utilitiesFetcher: container.injectOrFail(UtilitiesFetcher.self),
+//                notificationStatusProvider: container.injectOrFail(UNAuthorizationStatusProviding.self),
+//                databaseRepository: container.injectOrFail(MBDatabaseRepository.self),
+//                guaranteedDeliveryManager: container.injectOrFail(GuaranteedDeliveryManager.self),
+//                trackVisitManager: instanceFactory.makeTrackVisitManager(),
+//                sessionManager: container.injectOrFail(MBSessionManager.self),
+//                inAppMessagesManager: container.injectOrFail(InAppCoreManagerProtocol.self),
+//                uuidDebugService: container.injectOrFail(UUIDDebugService.self),
+//                userVisitManager: container.injectOrFail(UserVisitManagerProtocol.self))
+//        }
+//        
+//        return self
+//    }
 }
