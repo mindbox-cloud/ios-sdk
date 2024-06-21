dea9bd190397b66bdc5a562029fb83b3afd8117b
Akylbek Utekeshev <utekeshev@mindbox.cloud>
2024-06-21 17:16:57 +0400

    MBX-0000 DI Base

---
+++ Mindbox/DI/MBInject.swift
@@ -0,0 +1,59 @@
+//
+//  MBInject.swift
+//  Mindbox
+//
+//  Created by vailence on 21.06.2024.
+//  Copyright © 2024 Mindbox. All rights reserved.
+//
+
+import Foundation
+
+protocol ModuleInjecting {
+    func inject<Dependency>(_ serviceType: Dependency.Type) -> Dependency?
+    func injectOrFail<Dependency>(_ serviceType: Dependency.Type) -> Dependency
+}
+
+extension MBContainer: ModuleInjecting {
+    func inject<Dependency>(_ serviceType: Dependency.Type) -> Dependency? {
+        return self.resolve(serviceType)
+    }
+    
+    func injectOrFail<Dependency>(_ serviceType: Dependency.Type) -> Dependency {
+        return self.resolveOrFail(serviceType)
+    }
+}
+
+enum MBInject {
+    enum InjectionMode {
+        case standard
+        case test
+    }
+    
+    static var container: MBContainer = MBInject.buildDefaulContainer()
+    
+    static var mode: InjectionMode = .standard {
+        didSet {
+            switch mode {
+                case .standard:
+                    container = MBInject.buildDefaulContainer()
+                case .test:
+                    container = MBInject.buildTestContainer()
+            }
+        }
+    }
+    
+    fileprivate static func buildDefaulContainer() -> MBContainer {
+        let container = MBContainer()
+        return container
+    }
+    
+    public static var buildTestContainer: () -> MBContainer = {
+        let container = MBContainer()
+        return container
+    }
+}
+
+var container: ModuleInjecting {
+    return MBInject.container
+}
+
