70ebbaba90a7f1dce06c3a2962a8d4fa5d60fde3
Akylbek Utekeshev <utekeshev@mindbox.cloud>
2024-07-11 01:23:15 +0500

    MBX-0000 CoreController

--- Mindbox/DI/Injections/InjectCore.swift
+++ Mindbox/DI/Injections/InjectCore.swift
@@ -10,19 +10,17 @@ import Foundation
 
 extension MBContainer {
     func registerCore() -> Self {
-//        register(CoreController.self, scope: .container) {
-//            let instanceFactory = container.injectOrFail(InstanceFactory.self)
-//            return CoreController(
-//                persistenceStorage: container.injectOrFail(PersistenceStorage.self),
-//                utilitiesFetcher: container.injectOrFail(UtilitiesFetcher.self),
-//                databaseRepository: container.injectOrFail(MBDatabaseRepository.self),
-//                guaranteedDeliveryManager: container.injectOrFail(GuaranteedDeliveryManager.self),
-//                trackVisitManager: instanceFactory.makeTrackVisitManager(),
-//                sessionManager: container.injectOrFail(MBSessionManager.self),
-//                inAppMessagesManager: container.injectOrFail(InAppCoreManagerProtocol.self),
-//                uuidDebugService: container.injectOrFail(UUIDDebugService.self),
-//                userVisitManager: container.injectOrFail(UserVisitManagerProtocol.self))
-//        }
+        register(CoreController.self) {
+            CoreController(persistenceStorage: DI.injectOrFail(PersistenceStorage.self),
+                           utilitiesFetcher: DI.injectOrFail(UtilitiesFetcher.self),
+                           databaseRepository: DI.injectOrFail(MBDatabaseRepository.self),
+                           guaranteedDeliveryManager: DI.injectOrFail(GuaranteedDeliveryManager.self),
+                           trackVisitManager: DI.injectOrFail(TrackVisitManager.self),
+                           sessionManager: DI.injectOrFail(MBSessionManager.self),
+                           inAppMessagesManager: DI.injectOrFail(InAppCoreManagerProtocol.self),
+                           uuidDebugService: DI.injectOrFail(UUIDDebugService.self),
+                           userVisitManager: DI.injectOrFail(UserVisitManagerProtocol.self))
+        }
         
         register(GuaranteedDeliveryManager.self) {
             let persistenceStorage = DI.injectOrFail(PersistenceStorage.self)
@@ -31,8 +29,7 @@ extension MBContainer {
             return GuaranteedDeliveryManager(
                 persistenceStorage: persistenceStorage,
                 databaseRepository: databaseRepository,
-                eventRepository: eventRepository
-            )
+                eventRepository: eventRepository)
         }
         
         register(InAppConfigurationMapperProtocol.self) {
