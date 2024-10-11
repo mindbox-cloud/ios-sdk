7f53636ed082c405a215fafcb735774f19b8397c
justSmK <28645140+justSmK@users.noreply.github.com>
2024-10-11 12:25:06 +0300

    MBX-0000: Fix `@unchecked Sendable`
    Class must restate inherited '@unchecked Sendable' conformance

--- Mindbox/GuaranteedDeliveryManager/DeliveryOperation.swift
+++ Mindbox/GuaranteedDeliveryManager/DeliveryOperation.swift
@@ -9,7 +9,7 @@
 import Foundation
 import MindboxLogger
 
-class DeliveryOperation: AsyncOperation {
+class DeliveryOperation: AsyncOperation, @unchecked Sendable {
     private let event: Event
 
     private let databaseRepository: MBDatabaseRepository
