0fadb63a14bec1769f1778b8702f0099e27952ce
Akylbek Utekeshev <vailencethesupeman@gmail.com>
2025-04-23 13:35:13 +0500

    Feature/wmsdk 433 check product segment (#530)
    * WMSDK-433 Check targeting only if have product body
    
    * WMSDK-433 Sending CheckProductSegment if showing inapps
    
    * WMSDK-433 Updated targeting check logic
    
    * WMSDK-433 Added new logic
    
    * WMSDK-433 Updated old tests
    
    * WMSDK-433 Added new tests
    
    ---------
    
    Co-authored-by: Vailence <utekeshev@mindbox.cloud>

---
+++ Mindbox/InAppMessages/Models/DictionaryKeyValueModel.swift
@@ -0,0 +1,13 @@
+//
+//  DictionaryKeyValueModel.swift
+//  Mindbox
+//
+//  Created by Akylbek Utekeshev on 21.04.2025.
+//
+
+import Foundation
+
+struct DictionaryKeyValueModel: Hashable {
+    let key: String
+    let value: String
+}
