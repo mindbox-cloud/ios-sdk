40e8551d560364338f72f1a80cdefbe0cee7c7cd
Vailence <utekeshev@mindbox.cloud>
2025-06-09 16:30:03 +0500

    WMSDK-461 Updated timestamp as human readable log

--- Mindbox/Validators/InappPresentationValidator.swift
+++ Mindbox/Validators/InappPresentationValidator.swift
@@ -110,7 +110,7 @@ final class InAppPresentationValidator: InAppPresentationValidatorProtocol {
         let now = Date()
         let isAllowed = nextAllowedShowTime < now
         
-        Logger.common(message: "[PresentationValidator] lastShowDate: \(lastShowDate), minInterval: \(minInterval)s, now: \(now), Show allowed: \(isAllowed)", level: .info, category: .inAppMessages)
+        Logger.common(message: "[PresentationValidator] lastShowDate: \(lastShowDate.asDateTimeWithSeconds), minInterval: \(minInterval)s, nextAllowedShowTime: \(nextAllowedShowTime.asDateTimeWithSeconds), now: \(now.asDateTimeWithSeconds), Show allowed: \(isAllowed)", level: .info, category: .inAppMessages)
         return isAllowed
     }
 }
