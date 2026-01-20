f0c2c5fe72c54bc73d42ca75dd60cd6b9d96ddee
Akylbek Utekeshev <vailencethesupeman@gmail.com>
2026-01-20 17:49:45 +0800

    Feature/mobilewebview 3 spi web view facade (#637)
    * MOBILEWEBVIEW-3 Added Facade using spi
    
    * MOBILEWEBVIEW-3 Updated WebViewController
    
    * MOBILEWEBVIEW-3 Updated transparentView
    
    * MOBILEWEBVIEW-3 Added MindboxWebView
    
    * MOBILEWEBVIEW-3 PR Fix
    
    * MOBILEWEBVIEW-3 PR Fix Lint
    
    * MOBILEWEBVIEW-3 PR Fix naming
    
    ---------
    
    Co-authored-by: Vailence <utekeshev@mindbox.cloud>

---
+++ Mindbox/InAppMessages/Presentation/Views/WebView/MindboxWebView.swift
@@ -0,0 +1,138 @@
+//
+//  MindboxWebView.swift
+//  Mindbox
+//
+//  Created by vailence on 17.10.2024.
+//
+
+import UIKit
+import WebKit
+import MindboxLogger
+
+final class MindboxWebView: WKWebView {
+    static let sdkBridgeHandlerName = "SdkBridge"
+
+    init(
+        params: [String: String]?,
+        userAgent: String,
+        messageHandler: WKScriptMessageHandler,
+        navigationDelegate: WKNavigationDelegate?
+    ) {
+        let persistenceStorage = DI.injectOrFail(PersistenceStorage.self)
+
+        let contentController = WKUserContentController()
+        contentController.add(messageHandler, name: Self.sdkBridgeHandlerName)
+
+        let sdkBridgeParamsObjectString = Self.buildSdkBridgeParams(
+            params: params,
+            persistenceStorage: persistenceStorage
+        )
+
+        let jsObserver: String = """
+        window.webkit.messageHandlers.SdkBridge.postMessage({
+            'action': 'userAgent',
+            'data': navigator.userAgent
+        });
+
+        window.sdkBridgeParams = \(sdkBridgeParamsObjectString);
+
+        window.SdkBridge = {
+            receiveParam: function(paramName) {
+                if (typeof paramName !== 'string') return;
+                return window.sdkBridgeParams[paramName.toLowerCase()];
+            }
+        };
+        """
+
+        let userScript = WKUserScript(
+            source: jsObserver,
+            injectionTime: .atDocumentStart,
+            forMainFrameOnly: true
+        )
+
+        contentController.addUserScript(userScript)
+
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        config.applicationNameForUserAgent = userAgent
+
+        super.init(frame: .zero, configuration: config)
+
+        self.navigationDelegate = navigationDelegate
+
+        #if DEBUG
+        if #available(iOS 16.4, *) {
+            self.isInspectable = true
+        }
+        #endif
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private static func buildSdkBridgeParams(
+        params: [String: String]?,
+        persistenceStorage: PersistenceStorage
+    ) -> String {
+        var mindboxParams: [String: String] = [
+            "sdkVersion": Mindbox.shared.sdkVersion,
+            "endpointId": persistenceStorage.configuration?.endpoint ?? "",
+            "deviceUuid": persistenceStorage.deviceUUID ?? "",
+            "sdkVersionNumeric": "\(Constants.Versions.sdkVersionNumeric)"
+        ]
+
+        if let params, !params.isEmpty {
+            mindboxParams.merge(params) { _, new in new }
+        }
+
+        let lowercased = Dictionary(
+            uniqueKeysWithValues: mindboxParams.map { ($0.key.lowercased(), $0.value) }
+        )
+
+        guard let data = try? JSONSerialization.data(withJSONObject: lowercased),
+              let json = String(data: data, encoding: .utf8) else {
+            return "{}"
+        }
+        
+        return json
+    }
+
+    static func fetchHTMLContent(
+        from urlString: String,
+        completion: @escaping (String?) -> Void
+    ) {
+        guard let url = URL(string: urlString) else {
+            completion(nil)
+            return
+        }
+
+        let config = URLSessionConfiguration.ephemeral
+        config.requestCachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+        config.urlCache = nil
+
+        let session = URLSession(configuration: config)
+        let task = session.dataTask(with: url) { data, response, error in
+            if error != nil {
+                completion(nil)
+                return
+            }
+
+            guard let http = response as? HTTPURLResponse,
+                  (200...299).contains(http.statusCode) else {
+                completion(nil)
+                return
+            }
+
+            if let data,
+               let html = String(data: data, encoding: .utf8) {
+                completion(html)
+            } else {
+                completion(nil)
+            }
+        }
+
+        task.resume()
+    }
+}
---
+++ Mindbox/InAppMessages/Presentation/Views/WebView/TransparentView.swift
@@ -0,0 +1,186 @@
+//
+//  TransparentView.swift
+//  Mindbox
+//
+//  Created by vailence on 17.10.2024.
+//
+
+import UIKit
+import WebKit
+import MindboxLogger
+
+final class TransparentView: UIView {
+
+    weak var delegate: WebVCDelegate?
+    weak var webViewAction: WebViewAction?
+
+    private var facade: MindboxInternalWebViewFacadeProtocol?
+    private var quizInitTimeoutWorkItem: DispatchWorkItem?
+    private var params: [String: String]?
+    private let userAgent: String
+
+    init(frame: CGRect, params: [String: String], userAgent: String) {
+        self.params = params
+        self.userAgent = userAgent
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    override init(frame: CGRect) {
+        self.params = nil
+        self.userAgent = ""
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        self.params = nil
+        self.userAgent = ""
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    deinit {
+        Logger.common(message: "[WebView] Deinit TransparentView", category: .webViewInAppMessages)
+    }
+
+    private func commonInit() {
+        createFacade()
+
+        guard let view = facade?.makeView() else {
+            return
+        }
+        addSubview(view)
+        setupViewConstraints(view)
+
+        facade?.applyViewSettings(scrollViewDelegate: self)
+    }
+
+    private func createFacade() {
+        facade = MindboxInternalWebViewFacade(
+            params: params,
+            userAgent: userAgent,
+            messageHandler: self,
+            navigationDelegate: self
+        )
+    }
+
+    func loadHTMLPage(baseUrl: String, contentUrl: String) {
+        setupTimeoutTimer()
+
+        facade?.loadHTML(baseUrl: baseUrl, contentUrl: contentUrl) { [weak self] in
+            self?.quizInitTimeoutWorkItem?.cancel()
+            self?.delegate?.closeTimeoutWebViewVC()
+        }
+    }
+
+    func cleanUp() {
+        facade?.cleanWebView()
+    }
+
+    private func setupTimeoutTimer() {
+        let secondsTimeout = 7
+
+        quizInitTimeoutWorkItem?.cancel()
+        let workItem = DispatchWorkItem { [weak self] in
+            self?.delegate?.closeTimeoutWebViewVC()
+        }
+        quizInitTimeoutWorkItem = workItem
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(secondsTimeout), execute: workItem)
+    }
+}
+
+// MARK: - Constraints setup
+extension TransparentView {
+    private func setupViewConstraints(_ view: UIView) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: topAnchor),
+            view.bottomAnchor.constraint(equalTo: bottomAnchor),
+            view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+}
+
+// MARK: - WKScriptMessageHandler
+extension TransparentView: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        let name = message.name
+        
+        if name == "SdkBridge", let messageBody = message.body as? [String: String] {
+            let action = messageBody["action"] ?? "unknown"
+            let data = messageBody["data"] ?? ""
+            
+            Logger.common(message: "[WebView] TransparentWebView: SdkBridge - received \(action) \(data)", category: .webViewInAppMessages)
+            
+            switch action {
+            case "close":
+                quizInitTimeoutWorkItem?.cancel()
+                webViewAction?.onClose()
+            case "init":
+                quizInitTimeoutWorkItem?.cancel()
+                webViewAction?.onInit()
+            case "click":
+                webViewAction?.onCompleted(data: data)
+            case "hide":
+                webViewAction?.onHide()
+            case "log":
+                webViewAction?.onLog(message: data)
+            case "userAgent":
+                print("TransparentWebView: UserAgent: \(data)")
+            default:
+                Logger.common(message: "[WebView] TransparentWebView: action: \(action) with \(data)", category: .webViewInAppMessages)
+            }
+        }
+    }
+}
+
+// MARK: - WKNavigationDelegate
+
+extension TransparentView: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        Logger.common(message: "[WebView] WKNavigationDelegate: start loading URL \(webView.url?.absoluteString ?? "unknown")", category: .webViewInAppMessages)
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        Logger.common(message: "[WebView] WKNavigationDelegate: Upload completed \(webView.url?.absoluteString ?? "unknown")", category: .webViewInAppMessages)
+    }
+
+    func webView(
+        _ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping @MainActor (WKNavigationActionPolicy) -> Void
+    ) {
+        if let url = navigationAction.request.url {
+            Logger.common(message: "[WebView] WKNavigationDelegate: Navigating by URL \(url.absoluteString)", category: .webViewInAppMessages)
+        }
+        decisionHandler(.allow)
+    }
+
+    func webView(
+        _ webView: WKWebView,
+        didFailProvisionalNavigation navigation: WKNavigation!,
+        withError error: any Error
+    ) {
+        Logger.common(message: "[WebView] WKNavigationDelegate: Loading error \(error.localizedDescription)", category: .webViewInAppMessages)
+    }
+}
+
+extension TransparentView: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return nil
+    }
+}
+
+protocol WebViewAction: AnyObject {
+    func onInit()
+
+    func onCompleted(data: String)
+
+    func onClose()
+
+    func onHide()
+    
+    func onLog(message: String)
+}
