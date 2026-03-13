7e65d3c7b9cd23e372a769083e7d3e1877c61408
Vailence <utekeshev@mindbox.cloud>
2026-03-13 13:40:46 +0500

    MOBILEWEBVIEW-101: Add PermissionHandler protocol, result type, and registry

---
+++ Mindbox/InAppMessages/Presentation/Views/WebView/Bridge/Permissions/PermissionHandler.swift
@@ -0,0 +1,39 @@
+//
+//  PermissionHandler.swift
+//  Mindbox
+//
+//  Created by Akylbek Utekeshev on 13.03.2026.
+//  Copyright © 2026 Mindbox. All rights reserved.
+//
+
+import Foundation
+
+enum PermissionType: String {
+    case pushNotifications
+    case location
+}
+
+enum PermissionRequestResult {
+    case granted
+    case denied
+    case error(String)
+}
+
+protocol PermissionHandler {
+    var permissionType: PermissionType { get }
+    var requiredInfoPlistKeys: [String] { get }
+    func request(completion: @escaping (PermissionRequestResult) -> Void)
+}
+
+final class PermissionHandlerRegistry {
+
+    private var handlers: [PermissionType: PermissionHandler] = [:]
+
+    func register(_ handler: PermissionHandler) {
+        handlers[handler.permissionType] = handler
+    }
+
+    func handler(for type: PermissionType) -> PermissionHandler? {
+        handlers[type]
+    }
+}
