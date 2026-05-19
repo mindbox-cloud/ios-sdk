eaf34ff2330b0f8869028e4f3bbf2034533bc4b4
Vailence <utekeshev@mindbox.cloud>
2026-05-19 13:47:56 +0500

    MOBILE-182: Pin DateFormatter to en_US_POSIX in MindboxLogger ISO-8601 primitive

--- MindboxTests/Extensions/Tag+Extensions.swift
+++ MindboxTests/Extensions/Tag+Extensions.swift
@@ -27,4 +27,6 @@ extension Tag {
     @Tag static var trackVisit: Self
     @Tag static var operationsRouting: Self
     @Tag static var mbConfiguration: Self
+
+    @Tag static var dateFormatting: Self
 }
